69d5ed43a2e7a9dd5e944b1dbbabd3e6eb5af5ba
justinlazaro-iselect <justin.lazaro@iselect.com.au>
2020-02-20 12:37:25 +0800

    .

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewChecked,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef
+} from "@angular/core";
 
 @Component({
   selector: "app-server-element",
@@ -6,10 +21,55 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
   styleUrls: ["./server-element.component.css"],
   encapsulation: ViewEncapsulation.None
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy {
   @Input("svrElement") element: { type: string; name: string; content: string };
+  @Input() name: string;
+  @ViewChild("heading", { static: true }) header: ElementRef;
 
-  constructor() {}
+  constructor() {
+    console.log("constructor called!");
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges called!", changes);
+  }
+
+  ngOnInit(): void {
+    console.log("constructor ngOnInit!");
+    console.log("**", this.header.nativeElement.textContent);
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck called!");
+  }
+
+  ngAfterContentInit() {
+    console.log("AfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+    console.log("**1111", this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("OnDestroy");
+  }
 }
